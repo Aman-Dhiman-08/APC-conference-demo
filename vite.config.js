afe8c405b0f9376823baca90d15ef2c97b55f782
Aman <amandhiman130@gmai.com>
2026-04-13 21:49:40 +0530

    github pages fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/',
+  base: command === 'build' ? './' : '/',
   server: {
     proxy: {
       '/api': {
@@ -13,4 +13,6 @@ export default defineConfig({
       },
     },
   },
-})
+}))
+
+
