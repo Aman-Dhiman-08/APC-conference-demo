4e0cb448f76f71bd633324aff0253305c52c0300
Aman <amandhiman130@gmai.com>
2026-04-13 22:07:05 +0530

    v2 version

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DemoPage from './components/DemoPage'
+import DemoPageV2 from './components/DemoPageV2'
 
 function App() {
   return (
-    <DemoPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DemoPage />} />
+        <Route path="/v2" element={<DemoPageV2 />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
